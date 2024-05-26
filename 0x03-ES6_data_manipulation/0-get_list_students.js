863c9ed82575bf7d7005d259be956bdde3033b5e
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2024-05-27 00:16:19 +0300

    initial commit

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,8 @@
 export default function getListtudents() {
 	return [
-		{
-			id: 1,
-			firstName: "Guillame",
-			location: "San Francisco"
-		},
-		{
-			id: 2,
-			firstName: "James",
-			location: "Columbia"
-		},
-		{
-			id: 5,
-			firstname: "Serena",
-			location: "San Franscisco"
-		}
-	]
+		{ id: 1, firstName: "Guillame", location: "San Francisco" },
+		{ id: 2, firstName: "James", location: "Columbia" },
+		{ id: 5, firstname: "Serena", location: "San Franscisco" }
+	];
 
 }
