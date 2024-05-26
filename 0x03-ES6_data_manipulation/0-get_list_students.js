3ef4966d36cf744b3aec3b71a6253f4e09ab777a
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2024-05-26 23:56:58 +0300

    initial commit

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListtudents() {
+	const student = [
+		{
+			id: 1,
+			firstName: "Guillame",
+			location: "San Francisco"
+		},
+		{
+			id: 2,
+			firstName: "James",
+			location: "Columbia"
+		},
+		{
+			id: 5,
+			firstname: "Serena",
+			location: "San Franscisco"
+		}
+	]
+
+	return student;
+}
