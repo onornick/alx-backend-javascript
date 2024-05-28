1ef58b221d10c3dff5e14c861ad38f6fd3d6a6ba
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2024-05-28 23:05:24 +0300

    changed dataview to Int8Array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,10 +4,10 @@ export default function createInt8TypedArray(length, position, value) {
 	}
 	
 	const buffer = new ArrayBuffer(length);
-	const view = new DataView(buffer);
+	const int8 = new Int8Array(buffer, 0, length);
 
 	
-	view.setInt8(position, value);
+	int8.set([value], position);
 
-	return buffer;
+	return new DataView(buffer);
 }
