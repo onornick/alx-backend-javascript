4bfc48a50d13c9370fb5d966c5ea55a26a274598
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2024-05-28 22:54:32 +0300

    changed the error controller

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
+	if(position < 0 || position >= length){
+		throw new Error("Position outside range");
+	}
+	
 	const buffer = new ArrayBuffer(length);
 	const view = new DataView(buffer);
 
-	if (position < 0 || position >= length)
-		throw new Error("Position out of range");
 	
 	view.setInt8(position, value);
 
