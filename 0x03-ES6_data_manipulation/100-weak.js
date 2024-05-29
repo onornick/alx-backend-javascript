559e6fc0bbd6cf6872c1a59e5c5e6034dd3e09c7
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2024-05-29 12:52:04 +0300

    throw an error earlier in execution

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,11 @@
-/* eslint-disable no-param-reassign */
 export default function updateUniqueItems(map) {
-  if (map instanceof Map) {
-    for (const [key, value] of map) {
-      if (value === 1) {
-        map.set(key, 100);
-      }
-    }
-
-    return map;
-  }
-
-  throw new Error('Cannot process');
+	if (map !instanceof Map) {
+		throw new Error("Cannot process");
+	}
+	for (const [key, value] of map) {
+		if (value === 1) {
+			map.set(key, 100);
+			}
+		}
+	return map;
 }
