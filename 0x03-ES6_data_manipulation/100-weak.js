c159e93bf5c362585eba7211f961416dfc8d8d78
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2024-05-29 12:54:37 +0300

    changed the negation of instance

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(map) {
-	if (map !instanceof Map) {
+	if (!(map instanceof Map)) {
 		throw new Error("Cannot process");
 	}
 	for (const [key, value] of map) {
