0e7bac9cf07646c008aa016e3a81e07482b610cc
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2024-06-01 00:24:45 +0300

    re-implemented isDirector method

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -4,11 +4,13 @@ interface DirectorInterface {
 	workDirectorTasks(): string
 }
 
-interface TeacherInterface extends DirectorInterface {
+interface TeacherInterface {
+	workFromHome(): string,
+	getCoffeeBreak(): string,
 	workTeacherTasks(): string;
 }
 
-class Director implements DirectorInterface {
+export class Director implements DirectorInterface {
 	workFromHome(): string {
 		return "Working from home";
 	}
@@ -17,18 +19,22 @@ class Director implements DirectorInterface {
 		return "Getting a coffee break";
 	}
 
+	getCoffeeBreak(): string {
+		return "Cannot have a break";
+	}
+
 	workDirectorTasks(): string {
 		return "Getting to director tasks";
 	}
 }
 
-class Teacher implements TeacherInterface {
+export class Teacher implements TeacherInterface {
 	workFromHome(): string {
-		return "Working from home";
+		return "Cannot work from home";
 	}
 
 	getCoffeeBreak(): string {
-		return "Getting a coffee break";
+		return "Cannot have a break";
 	}
 
 	workTeacherTasks(): string {
@@ -37,8 +43,19 @@ class Teacher implements TeacherInterface {
 
 }
 
-export function createEmployee(salary: string | string) Teacher | Director {
+export function createEmployee(salary: string | number): Teacher | Director {
 	if (typeof(salary) == "number" && salary < 500 )
 		return new Teacher()
 	return new Director();
+}
+
+export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+	return employee instanceof Director;
+}
+
+export function executeWork(employee: DirectorInterface | TeacherInterface) {
+	if (employee instanceof Director)
+		employee.workDirectorTasks;
+	else if(employee instanceof Teacher)
+		employee.workTeacherTasks;
 }
